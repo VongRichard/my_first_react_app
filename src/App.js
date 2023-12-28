11c72a062e03040de710247051b0fafc71c0df72
Richard Vong <richard.vong192@gmail.com>
2023-12-28 20:34:04 +1100

    playing with props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 
 // Component
-
-const Person = () => {
+// Props object is passed as an argument in the component
+const Person = (props) => {
   return (
     <>
-    <h1>Name: John</h1>
-    <h2>Last Name: Doe</h2>
-    <h2>Age: 30</h2>
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h2>Age: {props.age}</h2>
     </>
   )
 }
@@ -17,11 +17,8 @@ const Person = () => {
 const App = () => {
   return (
     <div className="App">
-        <Person />
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <Person name={"John"} lastName={"Doe"} age={"25"}/>
+        <Person name={"Mary"} lastName={"Doe"} age={"24"}/>
     </div>
   );
 }
