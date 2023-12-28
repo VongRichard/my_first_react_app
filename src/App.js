afa56a5f5662be180310d953478732a88bda6cb4
Richard Vong <richard.vong192@gmail.com>
2023-12-28 21:05:54 +1100

    Playing around with Hooks, useEvents and States

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 // Component
 // Props object is passed as an argument in the component
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
+// State in react is a plain JavaSript object used to represent a piece 
+// of information about a components current situation. Managed by the component itself.
+// When you use a state, you basically need to use a hook as well.
+// Never mutate the state manually.
+
 
 // Functional Component
 // Arrow Functions are more popular than regular Function Declaration.
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  // Happens at the start when the page loads
+  useEffect(() => {
+    alert("You've changed the counter to " + counter);
+  }, [counter]);
+
   return (
     <div className="App">
-        <Person name={"John"} lastName={"Doe"} age={"25"}/>
-        <Person name={"Mary"} lastName={"Doe"} age={"24"}/>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
